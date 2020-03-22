25f94b62e62975d1d6a5c0768bfedfd043906b13
MartinKem <martin.kempken@online.de>
2020-03-22 09:45:07 +0100

    Working Access Code Generation

--- accessCodeServer/accessCodeServer.py
+++ accessCodeServer/accessCodeServer.py
@@ -41,33 +41,6 @@ class Serv(BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(bytes("404:" + self.path + "does not exist or may not be accessed", 'utf-8'))
 
-        # if self.path == "/":
-        #     self.path = "/index.html"
-        # public_resources = ["/index.html", "/Busplan.js", "/Busplan.css", "/favicon.ico", "/background_image2.jpg"]
-        #
-        # if public_resources.__contains__(self.path):
-        #     with open(self.path[1:], 'rb') as file:
-        #
-        #         content_type = "text/html; charset=UTF-8"
-        #         if self.path[-3:] == ".js":
-        #             content_type = "text/javascript"
-        #         elif self.path[-4:] == ".css":
-        #             content_type = "text/css; charset=UTF-8"
-        #         elif self.path[-4:] == ".ico":
-        #             content_type = "image/x-icon"
-        #         elif self.path[-4:] == ".jpg":
-        #             content_type = "image/jpeg"
-        #
-        #         self.send_response(200)
-        #         self.send_header("content-type", content_type)
-        #         self.end_headers()
-        #         self.wfile.write(file.read())
-        #
-        # else:
-        #     self.send_response(404)
-        #     self.end_headers()
-        #     self.wfile.write(bytes("404:" + self.path + "does not exist or may not be accessed", 'utf-8'))
-
     def do_POST(self):
 
         if self.path != "/index.html":
@@ -87,14 +60,6 @@ class Serv(BaseHTTPRequestHandler):
             accessCode = str(hash(content + rand))[-6:]
 
             xml_response = bytes("<accessCode>" + accessCode + "</accessCode>", 'utf-8')
-            # xml_response = ET.fromstring("<accessCode>" + accessCode + "</accessCode>")
-
-        if request_type == "trip":
-            xml_response = perform_trip_request(xml_body)
-
-        if request_type == "favourite_list":
-            xml_favourites = ET.parse('favourites.xml')
-            xml_response = ET.tostring(xml_favourites.getroot())
 
         self.send_response(200)
         self.send_header("content-type", "text/xml; charset=UTF-8")
