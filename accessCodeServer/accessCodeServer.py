9f8a9af52fb990cf43225680abf946faf01b5a56
MartinKem <martin.kempken@online.de>
2020-03-22 12:52:40 +0100

    Creating storage for access Codes + execution script for the server

--- accessCodeServer/accessCodeServer.py
+++ accessCodeServer/accessCodeServer.py
@@ -1,26 +1,11 @@
 import random
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import xml.etree.ElementTree as ET
+from urllib.parse import urlparse
+import sys
 
-import requests
-
-
-def perform_trip_request(xml_parameters):
-    first = True
-    base_url = "https://www.rmv.de/hapi/trip?"
-    for child in xml_parameters:
-        if not first:
-            base_url += "&"
-        else:
-            first = False
-        parameter_name = str(child.tag)
-        base_url += parameter_name
-        base_url += "="
-        base_url += str(child.text)
-
-    r = requests.get(url=base_url)
-    return r.content
-
+response_string0 = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>CallMyContacts - Corona Warnnachricht</title></head><body>Hier werden die Codes vergeben.<br><br><form action='/' method='post'><label for='warningMessage'>Zusätzliche Hinweise</label><br><textarea name='warningMessage' rows='2' id='warningMessage' placeholder='In diesem Feld können Hinweise bezüglich der lokalen Umstände gegeben werden ...' cols='20' style='width: 30%; height: 100px;'></textarea><br><br><input type='submit' value='Generiere Zugangscode'></form><p id='accessCode'>"
+response_string1 = "</p></body></html>"
 
 class Serv(BaseHTTPRequestHandler):
 
@@ -36,6 +21,13 @@ class Serv(BaseHTTPRequestHandler):
                 self.end_headers()
                 self.wfile.write(file.read())
 
+        elif self.path == "/requests.js":
+            with open(self.path[1:], 'rb') as file:
+                self.send_response(200)
+                self.send_header("content-type", "text/javascript; charset=UTF-8")
+                self.end_headers()
+                self.wfile.write(file.read())
+
         else:
             self.send_response(404)
             self.end_headers()
@@ -43,29 +35,40 @@ class Serv(BaseHTTPRequestHandler):
 
     def do_POST(self):
 
-        if self.path != "/index.html":
+        if self.path != "/":
             self.send_response(404)
             self.end_headers()
             self.wfile.write(bytes("404:" + self.path + " does not exist or is not accessible by a POST request", 'utf-8'))
 
         content_length = int(self.headers['Content-Length'])
-        body = str(self.rfile.read(content_length))[2:-1]
-        xml_body = ET.fromstring(body)
-        request_type = xml_body.attrib["type"]
+        body = self.rfile.read(content_length)
+
+        fields = urlparse(body).path.decode("utf-8")
 
         xml_response = bytes("empty", 'utf-8')
-        if request_type == "accessCode":
-            content = xml_body.text
-            rand = str(random.randrange(0, 1000000, 1))
-            accessCode = str(hash(content + rand))[-6:]
+        if fields.startswith("warningMessage="):
+            content = fields[len("warningMessage="):]
+            rand = str(random.randrange(0, 100000, 1))
+            access_code = str(hash(content + rand))[-6:]
 
-            xml_response = bytes("<accessCode>" + accessCode + "</accessCode>", 'utf-8')
+            store_new_code(access_code, content)
+            xml_response = bytes(response_string0 + access_code + response_string1, 'utf-8')
+
 
         self.send_response(200)
-        self.send_header("content-type", "text/xml; charset=UTF-8")
+        self.send_header("content-type", "text/html; charset=UTF-8")
         self.end_headers()
         self.wfile.write(xml_response)
 
 
-httpd = HTTPServer(('localhost', 8080), Serv)
+def store_new_code(access_code, content):
+    xml_code_list = ET.parse('accessCodes.xml')
+    root_node = xml_code_list.getroot()
+
+    ET.SubElement(root_node, "accessCode", {'content': content, 'access_code': access_code})
+    xml_code_list.write('accessCodes.xml')
+
+
+ip_address = sys.argv[1]
+httpd = HTTPServer((ip_address, 8080), Serv)
 httpd.serve_forever()
